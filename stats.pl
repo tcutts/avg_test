b86f418bfda5f23bb6d7b90371bedd14ea54a69d
Tim Cutts <tim.cutts@astrazeneca.com>
2024-07-12 12:47:29 +0100

    Update perl version and remove alternatives

--- stats.pl
+++ stats.pl
@@ -1,17 +1,28 @@
 #!/usr/bin/perl
-open F, "</tmp/data.txt";
 
-while (<F>) {
+use strict;
+use warnings;
+
+open(my $fh, "<:raw", "/tmp/data.bin") or die "Cannot open file $!";
+
+my @long_integers = ();
+my $buffer;
+while (read($fh, $buffer, 32768)) {
+    my @unpacked = unpack('(q<*)*', $buffer);  # Unpack several long integers from the buffer
+    push @long_integers, @unpacked;
+}
+close($fh);
+
+my $sum = 0;
+
+foreach (@long_integers) {
   $sum += $_;
-  $n++;
 }
 
-$mean = $sum/$n;
-seek(F, 0, 0);
+my $mean = $sum/scalar @long_integers;
 
 $sum = 0;
-while (<F>) {
+foreach (@long_integers) {
   $sum += ($_ - $mean)**2;
 }
-printf "Mean: %0.2f\nVariance: %0.2f\n", $mean, $sum/$n;
-
+printf "Mean: %0.2f\nVariance: %0.2f\n", $mean, $sum/scalar @long_integers;
